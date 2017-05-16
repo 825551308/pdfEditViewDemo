5e2bbf448761a721fa7aa5abb86dec674c1b49fb
jinshanshan <jinshanshan@jinshanshandeMac-mini.local>
2017-05-16 16:40:50 +0800

    本地修改

--- pdfEditViewDemo/ViewController.m
+++ pdfEditViewDemo/ViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    NSLog(@"test2");
     SSJPdfEditView *pdfEditView = [SSJPdfEditView instancePdfEditView];
     CGRect fm = self.view.bounds;
     pdfEditView.frame = fm;
